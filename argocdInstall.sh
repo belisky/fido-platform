9a31332b960cafe8b61dcc68272c3ce4f52dc0a3
Nobel Fiawornu <fiawornunobel@gmail.com>
2025-02-13 12:40:06 +0000

    updates commands to setup cluster and everything

--- argocdInstall.sh
+++ argocdInstall.sh
@@ -1,2 +1,9 @@
+k3d cluster create -c "/home/nobel/Downloads/platfrom-engineer-test-new (1)/platform-engineer-test/infra-k8s/k3d-config.yaml" --registry-config "/home/nobel/Downloads/platfrom-engineer-test-new (1)/platform-engineer-test/infra-k8s/k3d-registries.yaml"
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+sleep 180
+kubectl port-forward svc/argocd-server -n argocd 8085:443
+argocd admin initial-password -n argocd
+argocd cluster add k3d-fido-exam
+kubectl apply -f "/home/nobel/Downloads/platfrom-engineer-test-new (1)/platform-engineer-test/manifests/nginx-app.yaml"
+kubectl port-forward svc/nginx 8087:80
