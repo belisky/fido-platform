b5ca4f1ed580d8ba5483ca35f61c38714af3fc0f
Nobel Fiawornu <fiawornunobel@gmail.com>
2025-02-13 15:35:34 +0000

    tries local registry pull

--- argocdInstall.sh
+++ argocdInstall.sh
@@ -8,3 +8,10 @@ argocd cluster add k3d-fido-exam
 kubectl apply -f "/home/nobel/Downloads/platfrom-engineer-test-new (1)/platform-engineer-test/manifests/nginx-app.yaml"
 kubectl port-forward svc/nginx 8087:80
 kubectl create configmap ms-env --from-env-file=.env --dry-run=client -o yaml > configmap.yaml
+kubectl config set-context --current --namespace=argocd
+argocd cluster add k3d-fido-exam
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhcmdvY2QiLCJzdWIiOiJpbWFnZS11cGRhdGVyOmFwaUtleSIsIm5iZiI6MTczOTQ1ODEwMCwiaWF0IjoxNzM5NDU4MTAwLCJqdGkiOiJpbWFnZS11cGRhdGVyIn0.Osu4lbXvQAUlqxVPIpmQ-XoFrj7tGsmh2DHToLxgFfM
+
+docker pull nginx:latest
+docker tag nginx:latest k3d-my-service.local:44003/nginx:latest
+docker push k3d-registry.localhost:12345/nginx:latest
