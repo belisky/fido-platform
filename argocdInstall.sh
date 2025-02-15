e7bdda57ef629750dd856d51d1275f1ef6d299f4
Nobel Fiawornu <fiawornunobel@gmail.com>
2025-02-15 07:42:36 +0000

    updates common commands for setting up test environment

--- argocdInstall.sh
+++ argocdInstall.sh
@@ -1,4 +1,6 @@
+k3d registry create my-registry --port 5000
 k3d cluster create -c "/home/nobel/Downloads/platfrom-engineer-test-new (1)/platform-engineer-test/infra-k8s/k3d-config.yaml"
+# docker push
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 sleep 180
@@ -7,7 +9,8 @@ argocd admin initial-password -n argocd
 kubectl config set-context --current --namespace=argocd
 argocd cluster add k3d-fido-exam
 kubectl config set-context --current --namespace=default
-kubectl apply -f "/home/nobel/Downloads/platfrom-engineer-test-new (1)/platform-engineer-test/manifests/nginx-app.yaml"
+kubectl apply -f "/home/nobel/Downloads/platfrom-engineer-test-new (1)/platform-engineer-test/manifests/"
+kubectl apply -f "/home/nobel/Downloads/platfrom-engineer-test-new (1)/platform-engineer-test/manifests/nginx-helm.yaml"
 kubectl port-forward svc/nginx 8087:80
 cd "/home/nobel/Downloads/platfrom-engineer-test-new (1)/platform-engineer-test/my-service"
 kubectl create configmap ms-env --from-env-file=.env #--dry-run=client -o yaml > configmap.yaml
